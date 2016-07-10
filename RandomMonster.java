26abadb3f0df41ce58934bee6e5dc6e61f4131e1
TsujiRyota <acguy27@icloud.com>
2016-07-11 01:23:32 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class RandomMonster{
   void run(String[] arg){
-  
+  Map<Book, List<History>> historyMap = new HashMap<Book, List<History>>();
   
   }
   public static void main(String[] args){
