f7096cf671b3cc003dd4b86d0883a97418994cf9
SakataHaruka <g1544647@gmail.com>
2016-07-11 01:10:32 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -1,9 +1,12 @@
 import java.util.*;
 import java.io.*;
 public class RandomMonster{
+  void run(String[] arg){
   
   
+  }
   public static void main(String[] args){
-  
+  RandomMonster RM=new RandomMonster();
+  RM.run(arg);
   }
 }
